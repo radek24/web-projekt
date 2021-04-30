b82caf7e4a1a52f0fb7bb8cdcbf4052e0a7c32a8
radek24 <76497057+radek24@users.noreply.github.com>
2021-04-30 23:51:33 +0200

    užani nevim

--- javascript/mobilni-navigace.js
+++ javascript/mobilni-navigace.js
@@ -1,7 +1,12 @@
 function zobrazNavigaci() {
-    const navigace = document.querySelector("nav");
-    if (navigace.style.display == "block")
+    const navigace = document.querySelector("nav ul");
+    var img_burger = document.getElementById("burger_img");
+    if (navigace.style.display == "block") {
         navigace.style.display = ""; // lepší než "none"
-    else
+        img_burger.style.filter = " grayscale(100)";
+    } else {
         navigace.style.display = "block";
+
+        img_burger.style.filter = " grayscale(0)";
+    }
 }
--- javascript/shrink_nav_onscroll.js
+++ javascript/shrink_nav_onscroll.js
@@ -1,48 +1,22 @@
 window.onscroll = function() { scrollFunction() };
 var podnadpis = document.getElementsByTagName("h3")[0];
 var nadpis = document.getElementsByClassName("main_header")[0];
+var burger_nav = document.getElementsByClassName("burger")[0];
+var burger_img = document.getElementById("burger_img");
 
-/*
-var isMobile;
-
-function myFunction(x) {
-
-    if (x.matches) { // If media query matches
-        isMobile = true;
-        scrollFunction()
-    } else {
-        isMobile = false;
-        scrollFunction()
-    }
-}
-
-var x = window.matchMedia("(max-width: 800px)")
-myFunction(x) // Call listener function at run time
-x.addListener(myFunction) // Attach listener function on state changes
-*/
 function scrollFunction() {
 
-    //if (isMobile == false) {
     if (document.body.scrollTop > 70 || document.documentElement.scrollTop > 70) {
         nadpis.style.fontSize = "1.5em";
         nadpis.style.paddingTop = "10px";
         podnadpis.style.fontSize = "0px";
+        burger_nav.style.top = "17px";
+        burger_img.style.height = "20px";
     } else {
         nadpis.style.fontSize = "2em";
         nadpis.style.paddingTop = "30px";
         podnadpis.style.fontSize = "0.8em";
+        burger_nav.style.top = "40px";
+        burger_img.style.height = "25px";
     }
-    //}
-    /*
-        if (isMobile == true) {
-            if (document.body.scrollTop > 70 || document.documentElement.scrollTop > 70) {
-                nadpis.style.fontSize = "1.5em";
-                nadpis.style.paddingTop = "10px";
-                podnadpis.style.fontSize = "0px";
-            } else {
-                nadpis.style.fontSize = "2em";
-                nadpis.style.paddingTop = "30px";
-                podnadpis.style.fontSize = "0.8em";
-            }
-        }*/
 }
