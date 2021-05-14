4df32518da367708804a6140f873188ff85973d5
radek24 <76497057+radek24@users.noreply.github.com>
2021-05-14 15:44:39 +0200

    css

--- javascript/mobilni-navigace.js
+++ javascript/mobilni-navigace.js
@@ -1,11 +1,13 @@
 function zobrazNavigaci() {
     const navigace = document.querySelector("nav ul");
-    var img_burger = document.getElementById("burger_img");
-    if (navigace.style.display == "block") {
-        navigace.style.display = ""; // lepší než "none"
+    const img_burger = document.getElementById("burger_img");
+    if (img_burger.style.filter == "grayscale(0)") {
+        navigace.classList.toggle("max-height-class");
         img_burger.style.filter = " grayscale(100)";
     } else {
         navigace.style.display = "block";
-        img_burger.style.filter = " grayscale(0)";
+        navigace.classList.toggle("max-height-class");
+        navigace.style.maxHeight = navigace.scrollHeight + "px";
+        img_burger.style.filter = "grayscale(0)";
     }
 }
