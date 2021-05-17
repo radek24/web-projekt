dc12b10f36691ec8ee3d78d02efd73582dbd7d42
radek24 <76497057+radek24@users.noreply.github.com>
2021-05-17 09:04:09 +0200

    css

--- javascript/shrink_nav_onscroll.js
+++ javascript/shrink_nav_onscroll.js
@@ -3,6 +3,8 @@ var podnadpis = document.getElementsByTagName("h3")[0];
 var nadpis = document.getElementsByClassName("main_header")[0];
 var burger_nav = document.getElementsByClassName("burger")[0];
 var burger_img = document.getElementById("burger_img");
+var odkazy = document.querySelector("nav ul");
+
 
 function scrollFunction() {
 
@@ -12,11 +14,14 @@ function scrollFunction() {
         podnadpis.style.fontSize = "0px";
         burger_nav.style.top = "17px";
         burger_img.style.height = "20px";
+        odkazy.style.fontSize = "0.85em"
     } else {
         nadpis.style.fontSize = "2.3em";
         nadpis.style.paddingTop = "30px";
         podnadpis.style.fontSize = "0.8em";
         burger_nav.style.top = "40px";
         burger_img.style.height = "25px";
+
+        odkazy.style.fontSize = "1em"
     }
 }
